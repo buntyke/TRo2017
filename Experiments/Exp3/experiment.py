c54c4ff224cef3ff4954c12f46352127493b2519
Nishanth Koganti <buntyke@gmail.com>
2017-03-14 18:26:18 +0900

    [major] first commit

---
+++ Experiments/Exp3/experiment.py
@@ -0,0 +1,270 @@
+# hybrid.py: Python program to perform optimization on each frame
+# Author: Nishanth Koganti
+# Date: 2016/1/7
+
+# Updates to work on larger dataset
+# Date: 2016/09/16
+
+# import modules
+import os
+import GPy
+import csv
+import sys
+import time
+import random
+import filterpy
+import numpy as np
+import cPickle as pickle
+from matplotlib import cm
+import scipy.stats as stats
+from tabulate import tabulate
+import sklearn.metrics as metrics
+from sklearn import preprocessing
+import GPy.plotting.Tango as Tango
+from itertools import combinations
+from matplotlib import pyplot as plt
+from filterpy.kalman import KalmanFilter
+from sklearn.neighbors import NearestNeighbors
+from filterpy.kalman import UnscentedKalmanFilter as UKF
+
+def nnsearchFunc(mrdModel, testData):
+    # model variables
+    kKey = 'Cloud'
+    mKey = 'TopCoord'
+
+    qDim = mrdModel.X.mean.shape[1]
+    nDimIn = testData[kKey].shape[1]
+    nDimOut = testData[mKey].shape[1]
+    nSamples = testData[mKey].shape[0]
+    latentVals = np.zeros((nSamples,qDim))
+    predictVals = np.zeros((nSamples,nDimOut))
+
+    # obtain the training data latent positions
+    latentPositions = mrdModel.X.mean.values
+    nn = NearestNeighbors(n_neighbors=5,algorithm='kd_tree').fit(mrdModel.Y0.Y)
+
+    startTime = time.time()
+    for n in range(nSamples):
+        yIn = testData[kKey][n,:]
+        yTrueOut = testData[mKey][n,:]
+
+        _,indices = nn.kneighbors(np.atleast_2d(yIn))
+        latentVal = latentPositions[indices[0],:].mean(axis=0)
+
+        yOut = mrdModel.predict(np.atleast_2d(latentVal), Yindex=1)
+        latentVals[n,:] = latentVal
+        predictVals[n,:] = yOut[0]
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    stopTime = time.time()
+    print '\nFinished Strategy NN Search'
+
+    nrmse = np.divide(np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values')),
+                      testData[mKey].max(axis=0) - testData[mKey].min(axis=0))
+    rmse = np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values'))
+    corr = np.zeros((1,nDimOut))
+    for d in range(nDimOut):
+        corr[0,d],_ = stats.pearsonr(testData[mKey][:,d],predictVals[:,d])
+
+    results = {}
+    results['corr'] = corr
+    results['rmse'] = rmse
+    results['nrmse'] = nrmse
+    results['pred'] = predictVals
+    results['latent'] = latentVals
+    results['time'] = nSamples/(stopTime - startTime)
+    return results
+
+def optimizeFunc(mrdModel, testData):
+    # model variables
+    kKey = 'Cloud'
+    mKey = 'TopCoord'
+
+    qDim = mrdModel.X.mean.shape[1]
+    nDimIn = testData[kKey].shape[1]
+    nDimOut = testData[mKey].shape[1]
+    nSamples = testData[mKey].shape[0]
+    latentVals = np.zeros((nSamples,qDim))
+    predictVals = np.zeros((nSamples,nDimOut))
+
+    # obtain the training data latent positions
+    latentPositions = mrdModel.X.mean.values
+    nn = NearestNeighbors(n_neighbors=5,algorithm='kd_tree').fit(mrdModel.Y0.Y)
+
+    startTime = time.time()
+    for n in range(nSamples):
+        yIn = testData[kKey][n,:]
+        yTrueOut = testData[mKey][n,:]
+
+        [xPredict, infX] = mrdModel.Y0.infer_newX(yIn[None,:], optimize=True)
+        latentVal = xPredict.mean
+
+        yOut = mrdModel.predict(np.atleast_2d(latentVal), Yindex=1)
+        latentVals[n,:] = latentVal
+        predictVals[n,:] = yOut[0]
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    stopTime = time.time()
+    print '\nFinished Strategy Optimize'
+
+    nrmse = np.divide(np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values')),
+                      testData[mKey].max(axis=0) - testData[mKey].min(axis=0))
+    rmse = np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values'))
+    corr = np.zeros((1,nDimOut))
+    for d in range(nDimOut):
+        corr[0,d],_ = stats.pearsonr(testData[mKey][:,d],predictVals[:,d])
+
+    results = {}
+    results['corr'] = corr
+    results['rmse'] = rmse
+    results['nrmse'] = nrmse
+    results['pred'] = predictVals
+    results['latent'] = latentVals
+    results['time'] = nSamples/(stopTime - startTime)
+    return results
+
+def filterFunc(mrdModel, testData):
+    qDim = mrdModel.X.mean.values.shape[1]
+
+    scales1 = mrdModel.Y0.kern.input_sensitivity(summarize=False)
+    scales2 = mrdModel.Y1.kern.input_sensitivity(summarize=False)
+
+    scales1 = scales1/scales1.max()
+    scales2 = scales2/scales2.max()
+    
+    # get the number of dimensions
+    yThresh = 0.05
+    indices = np.asarray(range(qDim))
+    active1 = indices[scales1 >= yThresh]
+    active2 = indices[scales2 >= yThresh]
+    sharedDims = np.intersect1d(active2,active2)
+    nShared = len(sharedDims)
+
+    # get init latent state from optimization
+    hybridFPS = 10000.0
+    deltaT = 1.0/hybridFPS
+    
+    # state transition matrix
+    def f_cv(x, dt):
+        nShared = len(x)/2
+        F = np.eye(2*nShared)
+        F[:nShared,nShared:] = dt*np.eye(nShared)
+        return np.dot(F, x)
+
+    def h_cv(x):
+        nShared = len(x)/2
+        return x[:nShared]
+        
+    # create kalman filter
+    sigmas = filterpy.kalman.MerweScaledSigmaPoints(n=2*nShared, alpha=0.1, beta=2.0, kappa=1.0)
+    kf = UKF(dim_x=2*nShared, dim_z=nShared, fx=f_cv, hx=h_cv, dt=deltaT, points=sigmas)
+
+    # init state
+    yIn = testData['Cloud'][0,:]
+    [xPredict, infX] = mrdModel.Y0.infer_newX(yIn[None,:], optimize=True)
+    xPredict = xPredict.mean
+    
+    kf.x = np.zeros((2*nShared))
+    kf.x[:nShared] = xPredict[0,sharedDims]
+
+    # init covariance
+    kf.P *= 1e-4
+    
+    # process and measurement noise
+    kf.Q *= 1e-5
+    kf.R *= 1e-3
+    
+    # model variables
+    kKey = 'Cloud'
+    mKey = 'TopCoord'
+
+    qDim = mrdModel.X.mean.shape[1]
+    nDimIn = testData[kKey].shape[1]
+    nDimOut = testData[mKey].shape[1]
+    nSamples = testData[mKey].shape[0]
+    latentVals = np.zeros((nSamples,qDim))
+    predictVals = np.zeros((nSamples,nDimOut))
+
+    # obtain the training data latent positions
+    latentPositions = mrdModel.X.mean
+    nn = NearestNeighbors(n_neighbors=5,algorithm='kd_tree').fit(mrdModel.Y0.Y)
+
+    startTime = time.time()
+    for n in range(nSamples):
+        yIn = testData[kKey][n,:]
+        yTrueOut = testData[mKey][n,:]
+
+        kf.predict()
+        if n%hybridFPS == 0:
+            [xPredict, infX] = mrdModel.Y0.infer_newX(yIn[None,:], optimize=True)
+            xPredict = xPredict.mean
+            kf.update(xPredict[0,sharedDims], R=1e-6*np.eye(nShared))
+        else:
+            _,indices = nn.kneighbors(np.atleast_2d(yIn))
+            xPredict = latentPositions[indices[0],:].mean(axis=0)
+            kf.update(xPredict[sharedDims])
+        
+        # how to apply hybrid here??
+        # kalman filter
+        latentVal = np.atleast_2d(xPredict)    
+        latentVal[0,sharedDims] = kf.x[:nShared]
+        
+        yOut = mrdModel.predict(latentVal, Yindex=1)
+        latentVals[n,:] = latentVal
+        predictVals[n,:] = yOut[0]
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    stopTime = time.time()
+    print '\nFinished Strategy Hybrid'
+
+    nrmse = np.divide(np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values')),
+                      testData[mKey].max(axis=0) - testData[mKey].min(axis=0))
+    rmse = np.sqrt(metrics.mean_squared_error(testData[mKey],predictVals,multioutput='raw_values'))
+    corr = np.zeros((1,nDimOut))
+    for d in range(nDimOut):
+        corr[0,d],_ = stats.pearsonr(testData[mKey][:,d],predictVals[:,d])
+
+    results = {}
+    results['corr'] = corr
+    results['rmse'] = rmse
+    results['nrmse'] = nrmse
+    results['pred'] = predictVals
+    results['latent'] = latentVals
+    results['time'] = nSamples/(stopTime - startTime)
+    return results
+
+nShr = 4
+nPos = 6
+names = []
+dims = [1,7500,8]
+keys = ['Time','Cloud','TopCoord']
+
+for nS in range(nShr):
+    for nP in range(nPos):
+        names.append('K1S%dP%dT1' % (nS+1,nP+1))
+        
+Data = pickle.load(open('../Data/Data.p','rb'))
+
+experiments = ['Pos', 'Shr']
+functions = ['filter','nnsearch','optimize']        
+
+for nS in range(nShr):
+    for nP in range(nPos):
+        print 'Cycle:%d,%d' % (nS+1,nP+1)        
+        testInd = {'Pos':nS*nPos+nP, 'Shr':((nS+1)%nShr)*nPos+nP}
+        
+        # load mrd model and obtain plot indices
+        mrdModel = pickle.load(open('../Models/Model%d%d.p' % (nS+1,nP+1), 'rb'))
+        scales = 1./np.asarray(mrdModel.Y0.kern.lengthscale)
+        indices = scales.argsort()[-2:][::-1]
+            
+        for exp in experiments:
+            testData = {}
+            for key,dim in zip(keys,dims):
+                testData[key] = Data[names[testInd[exp]]][key]        
+        
+            results = {}
+            results['filter'] = filterFunc(mrdModel, testData)
+            results['nnsearch'] = nnsearchFunc(mrdModel, testData)
+            results['optimize'] = optimizeFunc(mrdModel, testData)
+            pickle.dump(results,open('Results/Res%s%d%d.p' % (exp,nS+1,nP+1), 'wb'))            
---
+++ Experiments/Exp9/markerPlot.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+# markerPlot.py: Plots to generate visualizations of cloth markers
+# Author: Nishanth Koganti
+# Date: 2016/11/6
+
+# import the modules
+import os
+import sys
+import GPy
+import csv
+import time
+import random
+import numpy as np
+import cPickle as pickle
+import matplotlib as mpl
+import matplotlib.cm as cm
+import scipy.stats as stats
+from tabulate import tabulate
+from GPy.plotting import Tango
+import sklearn.metrics as metrics
+from sklearn import preprocessing
+from itertools import combinations
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from GPy.plotting.matplot_dep.visualize import matplotlib_show
+from GPy.inference.latent_function_inference import InferenceMethodList, VarDTC, VarDTC_minibatch
+
+
+class cloth_show(matplotlib_show):
+    """Base class for visualizing motion capture data."""
+
+    def __init__(self, vals, limits, axes=None, connect=None, colors=np.array(12*['b'])):
+        if axes==None:
+            fig = plt.figure(facecolor='white')
+            axes = fig.add_subplot(111, projection='3d', aspect='equal')
+
+        if len(vals.shape)==1:
+            vals = vals[None,:]
+
+        self.fig = fig
+        matplotlib_show.__init__(self, vals, axes)
+
+        self.colors = colors
+        self.limits = limits
+        self.connect = connect
+
+        self.process_values()
+
+        self.initialize_axes()
+        self.axes.set_xticklabels([])
+        self.axes.set_yticklabels([])
+        self.axes.set_zticklabels([])
+        self.axes.set_xlabel('X Axis')
+        self.axes.set_ylabel('Y Axis')
+        self.axes.set_zlabel('Z Axis')
+
+        self.draw_edges()
+        self.draw_vertices()
+        self.axes.figure.canvas.draw()
+
+    def draw_vertices(self):
+        self.points_handle = self.axes.scatter(self.vals[:, 0], self.vals[:, 1], self.vals[:, 2], color=self.colors, s=40)
+
+    def draw_edges(self):
+        self.line_handle = []
+        if self.connect != None:
+            x = []
+            y = []
+            z = []
+            for item in connect:
+                x.append(self.vals[item[0], 0])
+                x.append(self.vals[item[1], 0])
+                x.append(np.NaN)
+                y.append(self.vals[item[0], 1])
+                y.append(self.vals[item[1], 1])
+                y.append(np.NaN)
+                z.append(self.vals[item[0], 2])
+                z.append(self.vals[item[1], 2])
+                z.append(np.NaN)
+            self.line_handle = self.axes.plot(np.array(x), np.array(y), np.array(z), '-', color='b', linewidth=3)
+
+    def modify(self, vals):
+        if len(vals.shape)==1:
+            vals = vals[None,:]
+        self.vals = vals.copy()
+
+        self.axes_modify()
+        self.process_values()
+
+        self.draw_edges()
+        self.draw_vertices()
+        self.axes.figure.canvas.draw()
+
+    def process_values(self):
+        self.vals = self.vals.T.reshape((self.vals.shape[1]/3, 3))
+
+    def initialize_axes(self, boundary=0.001):
+        """Set up the axes with the right limits and scaling."""
+        bs = [(self.limits[i,0]-self.limits[i,1])*boundary for i in range(3)]
+        self.axes.set_xlim([self.limits[0,1]-bs[0], self.limits[0,0]+bs[0]])
+        self.axes.set_ylim([self.limits[1,1]-bs[1], self.limits[1,0]+bs[1]])
+        self.axes.set_zlim([self.limits[2,1]-bs[2], self.limits[2,0]+bs[2]])
+
+    def axes_modify(self):
+        self.points_handle.remove()
+        if self.connect != None:
+            self.line_handle[0].remove()
+
+font = {'size':18}
+mpl.rc('font', **font)
+
+connect = [[0,1,'b'],[1,2,'b'],[2,3,'b'],[3,4,'b'],[4,5,'b'],[5,0,'b'],
+           [6,7,'g'],[7,8,'g'],[8,6,'g'],[9,10,'g'],[10,11,'g'],[11,9,'g']]
+
+model = pickle.load(open('Models/model.p','rb'))
+
+fig, (sense_axes, latent_axes) = plt.subplots(1,2,facecolor='white')
+
+plt.sca(latent_axes)
+model.plot_latent(ax=latent_axes,marker='o')
+latent_axes.set_xticks([])
+latent_axes.set_yticks([])
+
+trainData = model.Y.copy()
+limits = np.asarray([[trainData[:,0::3].max(),trainData[:,0::3].min()],
+                     [trainData[:,1::3].max(),trainData[:,1::3].min()],
+                     [trainData[:,2::3].max(),trainData[:,2::3].min()]])
+
+data_show = cloth_show(model.Y[:1, :].copy(), limits, connect=connect)
+dim_select = GPy.plotting.matplot_dep.visualize.lvm_dimselect(model.X.mean[:1, :].copy(), model, data_show,
+                                                              latent_axes=latent_axes, sense_axes=sense_axes)
+sense_axes.set_ylabel('ARD Weight', fontsize=15)
+sense_axes.set_xlabel('Latent Dimensions', fontsize=15)
+
+fig.canvas.draw()
+fig.tight_layout()
+
+data_show.fig.canvas.draw()
+data_show.fig.tight_layout()
+
+plt.show()
+raw_input('Press enter to finish')
